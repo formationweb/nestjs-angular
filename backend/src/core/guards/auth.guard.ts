027f9a86b604fca779431fb67ad25f2de242f711
RSamaium <sam@newbot.io>
2024-04-18 12:39:01 +0200

    auth guard fix

--- backend/src/core/guards/auth.guard.ts
+++ backend/src/core/guards/auth.guard.ts
@@ -1,15 +1,24 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
 import { UsersService } from "../../users/users.service";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
     constructor(
-        //private jwtService: JwtService,
+        private jwtService: JwtService,
         private userService: UsersService
     ) {}
 
     async canActivate(context: ExecutionContext): Promise<boolean> {
-        
+        const request = context.switchToHttp().getRequest()
+        const token = request.headers['x-access-token']
+        if (!token) {
+            return false
+        }
+        const decoded = await this.jwtService.verify(token)
+        const { userId } = decoded
+        request.user = await this.userService.findOne(userId)
+        delete request.user.password
         return true
     }
 }
