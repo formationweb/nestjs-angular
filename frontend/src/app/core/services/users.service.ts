daba202781bd589b9b45ae60d9392d9b360d1c09
RSamaium <sam@newbot.io>
2024-04-19 10:45:56 +0200

    users state

--- frontend/src/app/core/services/users.service.ts
+++ frontend/src/app/core/services/users.service.ts
@@ -1,24 +1,32 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable, inject } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
-import { User } from "../interfaces/user.interface";
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { User } from '../interfaces/user.interface';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
-export class UsersService { // store
-    readonly url = 'http://localhost:3000/api/users'
-    private _search$: BehaviorSubject<string> = new BehaviorSubject('') // state
-    readonly search$: Observable<string> = this._search$.asObservable() // getter ou selector
+export class UsersService {
+  // store
+  readonly url = 'http://localhost:3000/api/users';
+  private _search$: BehaviorSubject<string> = new BehaviorSubject(''); // state
+  readonly search$: Observable<string> = this._search$.asObservable(); // getter ou selector
 
-    //constructor(private http: HttpClient) {}
-    private http = inject(HttpClient)
+  private _users$: BehaviorSubject<User[]> = new BehaviorSubject([] as User[]);
+  readonly users$: Observable<User[]> = this._users$.asObservable();
 
-    setSearch(str: string) {
-        this._search$.next(str) // mutation
-    }
+  //constructor(private http: HttpClient) {}
+  private http = inject(HttpClient);
 
-    getAll(): Observable<User[]> {
-        return this.http.get<User[]>(this.url)
-    }
+  setSearch(str: string) {
+    this._search$.next(str); // mutation
+  }
+
+  getAll(): Observable<User[]> {
+    return this.http.get<User[]>(this.url).pipe(
+      tap((users) => {
+        this._users$.next(users);
+      })
+    );
+  }
 }
--- frontend/src/app/features/users/users.component.ts
+++ frontend/src/app/features/users/users.component.ts
@@ -1,6 +1,6 @@
-import { NgFor } from "@angular/common";
+import { AsyncPipe, NgFor } from "@angular/common";
 import { Component, OnDestroy, OnInit, inject } from "@angular/core";
-import { Subscription, interval } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 import { User } from "../../core/interfaces/user.interface";
 import { UsersService } from "../../core/services/users.service";
 
@@ -8,24 +8,17 @@ import { UsersService } from "../../core/services/users.service";
     selector: 'app-users',
     templateUrl: './users.component.html',
     standalone: true,
-    imports: [NgFor]
+    imports: [NgFor, AsyncPipe]
 })
 export class UsersComponent implements OnInit, OnDestroy {
     private userService = inject(UsersService)
-    users: User[] = []
+    users: Observable<User[]> = this.userService.users$
     subscription!: Subscription
 
     ngOnInit(): void {
-        this.subscription = interval(1000).subscribe((nb) => {
-            this.userService.getAll().subscribe({
-                next: (users: User[]) => {
-                    this.users = users
-                },
-                complete: () => {
-                    //console.log('finish')
-                }
-            })
-        }) 
+       // this.subscription = interval(1000).subscribe((nb) => {
+            this.userService.getAll().subscribe()
+       // }) 
         this.userService.search$.subscribe((str) => {
             console.log(str)
         })
